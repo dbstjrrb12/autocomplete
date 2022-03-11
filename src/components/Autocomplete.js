70c712f3814e984d69f9b3d91c8a7dffccb72ecf
yoonseokgyu <dbstjrrb1@gmail.com>
2022-03-11 17:24:34 +0900

    feat: Change prototype function
    1. change keydown event to input event
    
    - this event perceives real time input value change
    
    2. change setState function
    
    - add $button element depending on state (input element value)

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -3,53 +3,58 @@ const clearIcon = 'src/assets/icon/clear.svg';
 
 export const AutoComplete = (function () {
   let state = '';
+
+  const proto = AutoComplete.prototype;
   const $target = document.createElement('form');
   $target.classList.add('autocomplete');
 
-  function AutoComplete({ $app, initialState, onKeyDown }) {
+  const $button = document.createElement('button');
+  $button.classList.add('clear');
+
+  function AutoComplete({ $app, initialState, onInput }) {
     $app.appendChild($target);
 
     state = initialState;
-    AutoComplete.prototype.onKeyDown = onKeyDown;
+    proto.onInput = ({ target }) => {
+      onInput();
+      target.value ? proto.setState(true) : proto.setState(false);
+    };
 
     this.render();
   }
 
-  AutoComplete.prototype.onKeyDown = () => {};
-
-  AutoComplete.prototype.clearFunc = () => {
+  proto.clearFunc = ({ target }) => {
     const $inputElement = $target.querySelector('.autoInput');
 
     $inputElement.value = '';
     $inputElement.focus();
+
+    $target.removeChild(target);
   };
 
-  AutoComplete.prototype.setState = (newState) => {
+  proto.setState = (newState) => {
     state = newState;
-    render();
+
+    state
+      ? !$target.querySelector('.clear') && $target.appendChild($button)
+      : $target.removeChild($button);
   };
 
-  AutoComplete.prototype.render = () => {
+  proto.render = () => {
     $target.innerHTML = `
-                        <img class="findIcon" src=${findIcon} alt="돋보기">
-                        <label class="a11yhidden" for="autoinput">제목으로 검색</label> 
-                        <input class="autoInput" type="text" id="autoinput" placeholder="제목으로 검색" autocomplete="off"/>
-                        ${
-                          state
-                            ? `<button class="clear" type="button">
-                                <img src=${clearIcon} alt="입력 내용 삭제" />
-                               </button>`
-                            : ''
-                        }
-                        `;
+      <img class="findIcon" src=${findIcon} alt="돋보기">
+      <label class="a11yhidden" for="autoinput">제목으로 검색</label> 
+      <input class="autoInput" type="text" id="autoinput" placeholder="제목으로 검색" autocomplete="off"/>
+      ${state ? $button : ''}  
+    `;
 
-    $target
-      .querySelector('.clear')
-      .addEventListener('click', AutoComplete.prototype.clearFunc);
+    $button.addEventListener('click', proto.clearFunc);
+    $button.type = 'button';
+    $button.innerHTML = `<img src=${clearIcon} alt="입력 내용 삭제" />`;
 
     $target
       .querySelector('.autoInput')
-      .addEventListener('keydown', AutoComplete.prototype.onKeyDown);
+      .addEventListener('input', proto.onInput);
   };
 
   return AutoComplete;
