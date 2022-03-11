fd2efe337e233a635140383ca5a7914a8ae29510
yoonseokgyu <dbstjrrb1@gmail.com>
2022-03-11 14:16:06 +0900

    feat: Add Autocomplete component
    using Closure to hide state
    
    add prototype function
    - setState : change state from old to new & re-render
    - render : render html tag in target element

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,32 @@
+export const AutoComplete = (function () {
+  let state = '';
+  let $target = document.createElement('form');
+
+  function AutoComplete({ $app, initialState }) {
+    $app.appendChild($target);
+    state = initialState;
+    this.render();
+  }
+
+  AutoComplete.prototype.setState = (newState) => {
+    state = newState;
+    render();
+  };
+
+  AutoComplete.prototype.render = () => {
+    $target.innerHTML = `
+                        <img class="findIcon" src="" alt="돋보기">
+                        <label class="a11yhidden" for="autocomplete">검색창</label> 
+                        <input type="text" id="autocomplete" value=""/>
+                        ${
+                          state
+                            ? `<button type="button" class="clear">
+                                &times;
+                               </button>`
+                            : ''
+                        }
+                        `;
+  };
+
+  return AutoComplete;
+})();
